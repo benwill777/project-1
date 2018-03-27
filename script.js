3451818fa807fe937a31e68a400c74245cd37215
benwill777 <t.bennettwilliamsjr@gmail.com>
2018-03-27 16:55:35 -0400

    fixed the js link

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    alert("Do you want to play Jeopordy ? Start Playing by selecting a question in the game board after clicking OK button")
+
+    $("#music100").click(function () {
+        //alert("you have selected Music for 100 ")
+    });
+});
