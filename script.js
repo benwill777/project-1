c218107865a72fe26224ff7b3195a206a8abd628
benwill777 <t.bennettwilliamsjr@gmail.com>
2018-03-27 20:28:54 -0400

    added new submit button event listener, and gave functionality to all buttons in first category

--- script.js
+++ script.js
@@ -7,15 +7,27 @@ $(document).ready(function () {
 });
 totalPoints = 0;
 //get the element that pops open the modal
-let modalButton = document.getElementById('eats100');
+let modalButton1 = document.getElementById('eats100');
+let modalButton2 = document.getElementById('eats200');
+let modalButton3 = document.getElementById('eats300');
+let modalButton4 = document.getElementById('eats400');
+let modalButton5 = document.getElementById('eats500');
 
 let modal = document.getElementById('modalQues');
 //get close button
 let closeBtn = document.getElementById('closeBtn');
+let submitBtn = document.getElementById('submitButton');
 //listening for click 
-modalButton.addEventListener('click', openModal);
+modalButton1.addEventListener('click', openModal);
+modalButton2.addEventListener('click', openModal);
+modalButton3.addEventListener('click', openModal);
+modalButton4.addEventListener('click', openModal);
+modalButton5.addEventListener('click', openModal);
+
 //listening for click to close/submit modal
 closeBtn.addEventListener('click', closeModal);
+submitBtn.addEventListener('click', closeModal);
+
 
 function openModal() {
     // console.log(123)
