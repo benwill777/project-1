81cb5dcfef8e9ce72f1bcfb2d29b7d5ee45637ce
benwill777 <t.bennettwilliamsjr@gmail.com>
2018-03-28 14:47:20 -0400

    incorporated jquery to the project, button

--- script.js
+++ script.js
@@ -6,36 +6,8 @@ $(document).ready(function () {
     });
 });
 
-//get the element that pops open the modal
-// let modalButton = document.getElementById('eats100');
-
-
-// let modal = document.getElementById('modalQues');
-// //get close button
-// let closeBtn = document.getElementById('closeBtn');
-// let submitBtn = document.getElementById('submitButton');
-// //listening for click 
-// modalButton.addEventListener('click', openModal);
-
-
-// //listening for click to close/submit modal
-
-// closeBtn.addEventListener('click', closeModal);
-// submitBtn.addEventListener('click', closeModal);
-
-
-// function openModal() {
-//     // console.log(123)
-//     // change the modal's display from none to block
-//     modal.style.display = 'block';
-// }
-// // hides the modal 
-// function closeModal() {
-//     modal.style.display = 'none';
-// }
-
 let totalScore = 0;
-score = []
+let score = []
 
 let buttonQ1 = document.getElementById('eats100');
 //on click of button 1/1
@@ -50,8 +22,9 @@ function openPrompt() {
     } else if (question1 = 'the varsity') {
         alert("correct")
         totalScore = totalScore + 100
-        score.push = totalScore
-
         console.log(score)
     }
+    //find the "score" box
+    $('#points').remove.(totalScore)
+    //push in new text
 }
