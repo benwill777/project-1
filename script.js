0bd9d9538f6176d5802157af0c38b5c67961e410
benwill777 <t.bennettwilliamsjr@gmail.com>
2018-03-28 16:24:25 -0400

    i got the second button to work with the second function

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ let totalScore = 0;
 
 let buttonQ1 = document.getElementById('eats100');
 //on click of button 1/1
-buttonQ1.addEventListener('click', openPrompt);
+buttonQ1.addEventListener('click', openPrompt1);
 
-function openPrompt() {
+function openPrompt1() {
     let question1 = prompt("Which restaurant in atlanta asks you what'll you have?")
     if (question1 != 'the varsity') {
         alert("wrong")
@@ -28,3 +28,21 @@ function openPrompt() {
     $('#points').html('Total Points:' + totalScore)
     //push in new text
 }
+let buttonQ2 = document.getElementById('eats200');
+//on click of button 1/1
+buttonQ2.addEventListener('click', openPrompt2);
+
+function openPrompt2() {
+    let question2 = prompt("Which restaurant in Atlanta is on top of the Westin hotel?")
+    if (question2 != 'the sundial') {
+        alert("wrong")
+        totalScore = totalScore - 200
+        console.log(totalScore)
+    } else if (question1 = 'the sundial') {
+        alert("correct")
+        totalScore = totalScore + 200
+        console.log(totalScore)
+    }
+}
+//find the "score" box
+$('#points').html('Total Points:' + totalScore)
